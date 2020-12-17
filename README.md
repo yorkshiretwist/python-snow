702fe22123b9cea1f07259c6185846fd1c883d54
Chris Taylor <chris@stillbreathing.co.uk>
2020-12-17 13:33:09 +0000

    Updated readme with parameter instructions

--- README.md
+++ README.md
@@ -4,4 +4,28 @@ Simple python script which simulates snow falling.
 
 My son made a valiant effort to make a snow falling effect in python for a school project, but it didn't quite work. I had a go, maybe it's better.
 
+## Make it snow
+
 To run it execute: `python snow.py`
+
+## Options
+
+You can add parameters to the call to the `generate_snow()` function to change how the snow falls:
+
+`lines` (int): how many lines of snow (ahem) to draw. Default: 15.
+
+`width` (int): how many characters wide the snow is. Default: 70.
+
+`max_density` (int): the maximum number of snowflakes on each line. Default: 5.
+
+`delay` (float): the gap in seconds between drawing each line. Default: 0.1.
+
+For example, to simulate lots of snow falling slowly you can do this:
+
+`generate_snow(max_density=15, delay=0.3)`
+
+## Just for fun
+
+You can also pass in a `char` parameter which changes the character of the snowflake. Try using an emoji, like this:
+
+`generate_snow(char="💩")`
--- snow.py
+++ snow.py
@@ -1,22 +1,22 @@
 import random, time, os
 
-def generate_snow_line(width=70, max_density=5):
+def generate_snow_line(width=70, max_density=5, char="❆"):
    snow_line = []
    snow_flakes = []
    for i in range(0, random.randint(1, max_density)):
       snow_flakes.append(random.randint(0, width))
    for i in range(0, width):
       if i in snow_flakes: 
-         snow_line.append("❆")
+         snow_line.append(char)
       else:
          snow_line.append(" ")
    return "".join(snow_line)
 
-def generate_snow(lines=15, width=70, max_density=5, delay=0.1):
+def generate_snow(lines=15, width=70, max_density=5, delay=0.1, char="❆"):
     snow = []
     while True:
         for i in range(0, lines):
-            snow.insert(0, generate_snow_line(width, max_density))
+            snow.insert(0, generate_snow_line(width, max_density, char))
             os.system("cls")
             for line in snow:
                 print(line)
@@ -25,4 +25,4 @@ def generate_snow(lines=15, width=70, max_density=5, delay=0.1):
                 snow.pop(lines)
             
 os.system("cls")
-generate_snow()
+generate_snow(char="💩")
